05d8417098614e23fe172944fa56ec9485e031fd
Arturas <arturasrizgelis3@gmail.com>
2023-10-19 14:35:59 +0300

    PD5 solution

---
+++ PD5/JS/main.js
@@ -0,0 +1,13 @@
+// 1.
+let number = 1;
+console.log("Numeris: ", number);
+// 2. 
+let bigint = 87921302471312;
+console.log("bigint nmr: ", bigint);
+// 3. 
+let String = "aga"; 
+console.log("string: ", String);
+
+
+
+
---
+++ PD5/main.js
@@ -0,0 +1,27 @@
+// 1.
+let number = 1;
+console.log("Numeris: ", number);
+// 2. 
+let bigint = 87921302471312;
+console.log("bigint nmr: ", bigint);
+// 3. 
+let String = "aga"; 
+console.log("string: ", String);
+// 4.
+let True = true;
+let False = false;
+console.log("Tiesaa: ", True);
+console.log("Netiesaa: ", False);
+// 5.
+let Undefined;
+console.log("Undefind: ", Undefined);
+// 6.
+let Null = null;
+console.log("Nul: ", Null);
+// 7.
+let Object = 'csacas';
+console.log("MAksimas: ", Object);
+
+
+
+
