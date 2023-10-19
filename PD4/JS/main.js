9ad5c361f5c406e0b3cb075e0ed951226f669d83
Arturas <arturasrizgelis3@gmail.com>
2023-10-19 12:12:16 +0300

    PD4 solutions

---
+++ PD4/JS/main.js
@@ -0,0 +1,19 @@
+// 1.
+let petName;
+let petAge;
+// 2.
+petName="Puzik";
+petAge="14";
+// 3.
+console.log("Pet Name: " + petName)
+console.log("Pet Age: " + petAge)
+// 4.
+petName="slavik";
+petAge="10";
+// 5.
+const buildingAddres="statybininku 7";
+// 6. 
+console.log("building Addres: " + buildingAddres);
+
+
+
